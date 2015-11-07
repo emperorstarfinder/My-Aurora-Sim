d3b2536980786351ff5e901669f8c8afb9d04950
EmperorStarfinder <emperor@secondgalaxy.com>
2015-11-06 22:19:48 -0500

    Clean out dirty initial code part 2
    - This adds cleaned up code

---
+++ Aurora/Region/AsyncSceneLoader.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+
+using Aurora.Framework.ModuleLoader;
+using Aurora.Framework.Modules;
+using Aurora.Framework.SceneInfo;
+using Aurora.Framework.Services;
+using Nini.Config;
+using Aurora.Framework.Servers;
+
+namespace Aurora.Region
+{
+    public class AsyncSceneLoader : ISceneLoader, IApplicationPlugin
+    {
+        private IConfigSource m_configSource;
+        private ISimulationBase m_simBase;
+
+        #region IApplicationPlugin Members
+
+        public void PreStartup(ISimulationBase simBase)
+        {
+        }
+
+        public void Initialize(ISimulationBase openSim)
+        {
+            m_simBase = openSim;
+            m_configSource = openSim.ConfigSource;
+
+            bool enabled = false;
+            if (m_simBase.ConfigSource.Configs["SceneLoader"] != null)
+                enabled = m_simBase.ConfigSource.Configs["SceneLoader"].GetBoolean("AsyncSceneLoader", false);
+
+            if (enabled)
+                m_simBase.ApplicationRegistry.RegisterModuleInterface<ISceneLoader>(this);
+        }
+
+        public void PostInitialise()
+        {
+        }
+
+        public void Start()
+        {
+        }
+
+        public void PostStart()
+        {
+        }
+
+        public void Close()
+        {
+        }
+
+        public void ReloadConfiguration(IConfigSource m_config)
+        {
+        }
+
+        #endregion
+
+        #region ISceneLoader Members
+
+        public string Name
+        {
+            get { return "AsyncSceneLoader"; }
+        }
+
+        /// <summary>
+        ///     Create a scene and its initial base structures.
+        /// </summary>
+        /// <param name="regionInfo"></param>
+        /// <param name="configSource"></param>
+        /// <returns></returns>
+        public IScene CreateScene(ISimulationDataStore dataStore, RegionInfo regionInfo)
+        {
+            AgentCircuitManager circuitManager = new AgentCircuitManager();
+            List<IClientNetworkServer> clientServers = AuroraModuleLoader.PickupModules<IClientNetworkServer>();
+            List<IClientNetworkServer> allClientServers = new List<IClientNetworkServer>();
+            foreach (IClientNetworkServer clientServer in clientServers)
+            {
+                clientServer.Initialise((uint)regionInfo.RegionPort, m_configSource, circuitManager);
+                allClientServers.Add(clientServer);
+            }
+
+            AsyncScene scene = new AsyncScene();
+            scene.AddModuleInterfaces(m_simBase.ApplicationRegistry.GetInterfaces());
+            scene.Initialize(regionInfo, dataStore, circuitManager, allClientServers);
+
+            return scene;
+        }
+
+        #endregion
+    }
+}
---
+++ Compile.VS2010.net4_0.AnyCPU.debug.bat
@@ -0,0 +1,1 @@
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild Aurora.sln  /p:Configuration=Debug  /p:DefineConstants="ISWIN;NET_4_0"
